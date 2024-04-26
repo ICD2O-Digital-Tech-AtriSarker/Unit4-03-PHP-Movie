fede4b7a66672f599a6db5888e76d34c26daa78f
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-04-26 15:47:43 +0000

    Initial

--- index.php
+++ index.php
@@ -1,9 +1,66 @@
-
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <p>Paragraph</p>
-  </body>
+
+<head>
+  <!-- Meta data section -->
+  <meta charset="utf-8">
+  <meta name="description" content="widget that takes in years of age as an input and outputs what rating of movies an individual of the specified age is able to see, Using PHP">
+  <meta name="keywords" content="immaculata, icd2o">
+  <meta name="author" content="Atri Sarker">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/favicon-16x16.png">
+  <link rel="manifest" href="./fav_index/site.webmanifest">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./css/style.css">
+
+  <!-- MDL -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+  <!-- Title -->
+  <title>Movie Age Rating Checker in PHP</title>
+</head>
+
+<body>
+
+  <!-- Header -->
+  <h1>Movie Age Rating Checker in PHP</h1>
+
+  <!-- INPUT FORM [used for form validation] -->
+  <form id="inputForm" method="post" action="results.php" target="resultFrame">
+    <!-- Table For Layout -->
+    <table>
+      <tr>
+        <td>
+          <!--  age Input -->
+          <label for="ageInput">Enter age ( years ):</label>
+          <!-- Step, Min, and Max are undefined for sake of assignment -->
+          <input id="ageInput" type="number" name="age" value="" step="any" min="" max="" />
+          <br>
+        </td>
+        <td>
+          <!-- Image of PHP LOGO -->
+          <img class="contentImg" src="./images/phpLogo.svg" alt="php Logo!">
+          <br>
+        </td>
+      </tr>
+    </table>
+
+    <!-- Results Button -->
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit"
+      value="submit" id="resultBtn">Get Result!</button>
+  </form>
+
+  <!-- Result -->
+  <iframe name="resultFrame" src="./results.php">
+  </iframe>
+
+</body>
+
 </html>
--- results.php
+++ results.php
@@ -1,16 +1,44 @@
 
-<!-- Result -->
-  <iframe name="resultFrame" src="./results.php">
-  </iframe>
-<!-- <form id="inputForm" method="post" action="results.php" target="resultFrame"> -->
-
 <b>
   <?php
     // Code
-    if(isset($_POST['submit'])) 
+    if(isset($_POST['age'])) 
     {
-      // INPUT
-      $input = floatval($_POST['{value}']);
+      // Get user input, years of age
+      $age = floatval($_POST['age']);
+
+      // When the user’s age allows them to go to an R-rated movie alone
+      // When the user’s age allows them to go to a PG-13 movie alone
+      // When the user’s age allows them to go to a G or PG movie alone
+      // When the user’s age does not allow them to go to a movie alone
+      // When the user’s age is a decimal, web page should not allow this entry
+      // When the user’s age is a negative number, web page should not allow this entry
+      // When nothing is entered, web page should ask them to please input their age.
+      $message = "";
+      if (is_nan($age)) {
+        $message = "Please Enter Input!";
+      }
+      else if ($age <= 0) {
+        $message = "Please Enter Postive Input!";
+      }
+      else if (str_contains(strval($age), ".")) {
+        $message = "Please Enter Positive Integer!";
+      }
+      else if ($age >= 17) {
+        $message = "Allowed to see R-rated movies alone!";
+      }
+      else if ($age >= 13) {
+        $message = "Allowed to see PG-13 movies alone!";
+      }
+      else if ($age >= 8) {
+        $message = "Allowed to see PG movies alone!";
+      }
+      else {
+        $message = "Not allowed to see movies alone!";
+      }
+
+      // Display Result
+      echo $message;
     }
     else {
       // Default
